0f9334bb77ac49514291693371c92e14fc66e0a5
ABHISHEK TOMAR <103330372+Abhishek1044@users.noreply.github.com>
2022-06-08 21:26:42 +0530

    Update ques1.py

--- ques1.py
+++ ques1.py
@@ -1,19 +1,13 @@
-# def reverse(s):
-#     str = ""
-#     for i in s:
-#         str = i + str
-#     return str
+def reverse(s):
+    str = ""
+    for i in s:
+        str = i + str
+    return str
 
-# s = "PunjabEnggCollege"
+s = "PunjabEnggCollege"
 
-# print ("The original string is : ",end="")
-# print (s)
+print ("The original string is : ",end="")
+print (s)
 
-# print ("The reversed string(using loops) is : ",end="")
-# print (reverse(s))
-
-string="hello ! world "
-for i in string:
-    string[int(i)]=string[int(-i)]
-    
-print(string)
+print ("The reversed string(using loops) is : ",end="")
+print (reverse(s))
